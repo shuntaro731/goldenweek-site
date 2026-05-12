8d521b59097c16b0df9996fcfd79a590bf4bf729
shuntaro <shuntaro.hata0731@gmail.com>
2026-05-12 12:15:30 +0900

    feat: MagicUIを依存関係に追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
+
+  integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
